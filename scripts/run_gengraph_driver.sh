dd34bf021395a86d7afea2ac236e48e7da1f586d
fvahedian <fatima.vahedian@gmail.com>
2021-03-02 11:43:45 -0500

    Update run_gengraph_driver.sh

--- scripts/run_gengraph_driver.sh
+++ scripts/run_gengraph_driver.sh
@@ -3,5 +3,5 @@ time python ../GenGraph/driver.py \
       --output_path='/y/vfatemeh/NN/ExpWinter2021/exp/exp_lr_3.5e-06_momentum_9_dr_8_bs_128_ne_100_sf_10_sgd_AUGDATA_sbmb_0' \
       --weight_transform='identity'\
       --glob='False'\
-      --num_convert=100
+      --num_convert=10
 
