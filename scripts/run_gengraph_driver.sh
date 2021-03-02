abdeea056bef8abf5b8cbcad826b58893a7c0451
fvahedian <fatima.vahedian@gmail.com>
2021-03-02 11:43:23 -0500

    Update run_gengraph_driver.sh

--- scripts/run_gengraph_driver.sh
+++ scripts/run_gengraph_driver.sh
@@ -1,6 +1,6 @@
 time python ../GenGraph/driver.py \
-      --input_path='/y/vfatemeh/NN/ExpWinter2021/exp/exp_lr_1.5e-05_momentum_9_dr_4_bs_128_ne_100_sf_10_sgd_AUGDATA_sbmb_0' \
-      --output_path='/y/vfatemeh/NN/ExpWinter2021/exp/exp_lr_1.5e-05_momentum_9_dr_4_bs_128_ne_100_sf_10_sgd_AUGDATA_sbmb_0' \
+      --input_path='/y/vfatemeh/NN/ExpWinter2021/exp/exp_lr_3.5e-06_momentum_9_dr_8_bs_128_ne_100_sf_10_sgd_AUGDATA_sbmb_0' \
+      --output_path='/y/vfatemeh/NN/ExpWinter2021/exp/exp_lr_3.5e-06_momentum_9_dr_8_bs_128_ne_100_sf_10_sgd_AUGDATA_sbmb_0' \
       --weight_transform='identity'\
       --glob='False'\
       --num_convert=100
