1e27af682fb2f1f4475ebb39ea1cbe98483372db
Ruiyu Li <ruiyuli@umich.edu>
2022-08-21 02:50:25 -0400

    clean files

--- GenData/AlexNet.py
+++ GenData/AlexNet.py
@@ -54,13 +54,65 @@ class EarlyStopping(object):
 
 
 # AlexNet with dropout
-class Net(nn.Module):
-    def __init__(self, num_classes=10, init_weights=False, p=0.5):
+class Net10(nn.Module):
+    def __init__(self, num_classes=10, init_weights=True, p=0.5):
         # trained on 2 GPUs, change conv size if trained on CPU
         # trained on CIFAR10 so changed some sizes
         super(Net, self).__init__()
         self.p = p
 
+        self.conv1 = nn.Conv2d(3, 48 * 2, kernel_size=7, stride=2, padding=2)  # input[3, 32, 32]  output[48, 15, 15]
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)  # output[48, 27, 27]
+        self.conv2 = nn.Conv2d(48 * 2, 128 * 2, kernel_size=5, padding=2)  # output[128, 27, 27]
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)  # output[128, 13, 13]
+
+        self.conv3 = nn.Conv2d(128 * 2, 192 * 2, kernel_size=3, padding=1)  # output[192, 13, 13]
+        self.conv4 = nn.Conv2d(192 * 2, 192 * 2, kernel_size=3, padding=1)  # output[192, 13, 13]
+        self.conv5 = nn.Conv2d(192 * 2, 128 * 2, kernel_size=3, padding=1)  # output[128, 13, 13]
+
+        self.dr1 = nn.Dropout(p=self.p)
+        self.fc1 = nn.Linear(128 * 2 * 3 * 3, 1024)
+        self.dr2 = nn.Dropout(p=self.p)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, num_classes)
+
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = torch.flatten(x, start_dim=1)
+
+        x = F.relu(self.fc1(self.dr1(x)))
+        x = F.relu(self.fc2(self.dr2(x)))
+        x = self.fc3(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+# AlexNet with dropout
+class Net(nn.Module):
+    def __init__(self, num_classes=100, init_weights=True, p=0.5):
+        # trained on 2 GPUs, change conv size if trained on CPU
+        # trained on CIFAR10 so changed some sizes
+        super(Net, self).__init__()
+        self.p = p
+        print("Dropout net -- {}".format(p))
         self.conv1 = nn.Conv2d(3, 48 * 2, kernel_size=7, stride=2, padding=2)  # input[3, 32, 32]  output[48, 15, 15]
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)  # output[48, 27, 27]
         self.conv2 = nn.Conv2d(48 * 2, 128 * 2, kernel_size=5, padding=2)  # output[128, 27, 27]
@@ -296,3 +348,68 @@ def get_cifar_data(augment_data=False, batch_size=128):
         )
 
     return trainloader, testloader, classes
+
+
+# data function
+def get_imagenet_data(augment_data=False, batch_size=128):
+    train_path = "/cluster/home/it_stu187/iris/data/tiny-imagenet-200/train"
+    test_path = "/cluster/home/it_stu187/iris/data/tiny-imagenet-200/test"
+    if augment_data:
+        transform_train = transforms.Compose(
+            [
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+        # Normalize the test set same as training set without augmentation
+        transform_test = transforms.Compose(
+            [
+                transforms.Resize(size=(32, 32)),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+        # flip, rotate, crop
+
+        trainset = torchvision.datasets.ImageFolder(
+            root=train_path, transform=transform_train
+        )
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=batch_size, shuffle=True, num_workers=2
+        )
+
+        testset = torchvision.datasets.ImageFolder(
+            root=test_path, transform=transform_test
+        )
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=batch_size, shuffle=False, num_workers=2
+        )
+
+    else:
+        # return normal dataloaders
+        transform = transforms.Compose(
+            [
+                transforms.Resize(size=(32, 32)),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+        trainset = torchvision.datasets.ImageFolder(
+            root=train_path, transform=transform
+        )
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=batch_size, shuffle=True, num_workers=2
+        )
+
+        testset = torchvision.datasets.ImageFolder(
+            root=test_path, transform=transform
+        )
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=batch_size, shuffle=False, num_workers=2
+        )
+
+    return trainloader, testloader
--- GenData/VGG.py
+++ GenData/VGG.py
@@ -53,9 +53,9 @@ class EarlyStopping(object):
                 self.is_better = lambda a, best: a > best + (best * min_delta / 100)
 
 
-class Net(nn.Module):
+class Net32(nn.Module):
     def __init__(self, num_classes=10, init_weights=False, p=0.5):
-        super(Net, self).__init__()
+        super(Net32, self).__init__()
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
@@ -74,6 +74,65 @@ class Net(nn.Module):
         self.dr2 = nn.Dropout(p=p)
         self.fc2 = nn.Linear(128, 128)
         self.fc3 = nn.Linear(128, num_classes)
+
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        # N x 3 x 224 x 224
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = self.pool2(x)
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = self.pool3(x)
+        x = F.relu(self.conv6(x))
+        x = F.relu(self.conv7(x))
+        x = self.pool4(x)
+        # N x 512 x 7 x 7
+        x = torch.flatten(x, start_dim=1)
+        # N x 512*7*7
+        x = self.dr1(F.relu(self.fc1(x)))
+        x = self.dr2(F.relu(self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                # nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+class Net(nn.Module):
+    def __init__(self, num_classes=100, init_weights=False, p=0.5):
+        super(Net, self).__init__()
+        print("Dropout net -- {}".format(p))
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+
+        self.dr1 = nn.Dropout(p=p)
+        self.fc1 = nn.Linear(256*2*2, 512)
+        self.dr2 = nn.Dropout(p=p)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, num_classes)
 
         if init_weights:
             self._initialize_weights()
@@ -218,7 +277,7 @@ class BatchNorm_Net(nn.Module):
         return x, saved_vals
 
 
-# data function
+# data function1
 def get_cifar_data(augment_data=False, batch_size=128):
     if augment_data:
         transform_train = transforms.Compose(
@@ -303,3 +362,129 @@ def get_cifar_data(augment_data=False, batch_size=128):
         )
 
     return trainloader, testloader, classes
+
+
+# data function2
+def get_fMnist_data(augment_data=False, batch_size=128):
+    if augment_data:
+        transform_train = transforms.Compose(
+            [
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+        # Normalize the test set same as training set without augmentation
+        transform_test = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+        # flip, rotate, crop
+
+        trainset = torchvision.datasets.FashionMNIST(
+            root="../data", train=True, download=True, transform=transform_train
+        )
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=batch_size, shuffle=True, num_workers=2
+        )
+
+        testset = torchvision.datasets.FashionMNIST(
+            root="../data", train=False, download=True, transform=transform_test
+        )
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=batch_size, shuffle=False, num_workers=2
+        )
+
+    else:
+        # return normal dataloaders
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+        trainset = torchvision.datasets.FashionMNIST(
+            root="../data", train=True, download=True, transform=transform
+        )
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=batch_size, shuffle=True, num_workers=2
+        )
+
+        testset = torchvision.datasets.FashionMNIST(
+            root="../data", train=False, download=True, transform=transform
+        )
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=batch_size, shuffle=False, num_workers=2
+        )
+
+    return trainloader, testloader
+
+
+# data function3
+def get_imagenet_data(augment_data=False, batch_size=128):
+    train_path = "../../data/tiny-imagenet-200/train"
+    test_path = "../../data/tiny-imagenet-200/test"
+    if augment_data:
+        transform_train = transforms.Compose(
+            [
+                transforms.Resize(size=(32, 32)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+
+        # Normalize the test set same as training set without augmentation
+        transform_test = transforms.Compose(
+            [
+                transforms.Resize(size=(32, 32)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        # flip, rotate, crop
+
+        trainset = torchvision.datasets.ImageFolder(
+            root=train_path, transform=transform_train
+        )
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=batch_size, shuffle=True, num_workers=2
+        )
+
+        testset = torchvision.datasets.ImageFolder(
+            root=test_path, transform=transform_test
+        )
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=batch_size, shuffle=False, num_workers=2
+        )
+
+    else:
+        # return normal dataloaders
+        transform = transforms.Compose(
+            [
+                transforms.Resize(size=(32, 32)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+
+        trainset = torchvision.datasets.ImageFolder(
+            root=train_path, transform=transform
+        )
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=batch_size, shuffle=True, num_workers=2
+        )
+
+        testset = torchvision.datasets.ImageFolder(
+            root=test_path, transform=transform
+        )
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=batch_size, shuffle=False, num_workers=2
+        )
+
+    return trainloader, testloader
